1d8e25f31b2d6b36da1ed8253985e6f5b9e6f1af
Dutch Players <info@dutch-players.nl>
2021-04-05 02:40:55 +0200

    Merged pull request https://github.com/german-admiral-man/Impound-UI/pull/2

--- client/main.lua
+++ client/main.lua
@@ -1,16 +1,3 @@
-local Keys = {
-    ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-    ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-    ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-    ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-    ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-    ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-    ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-    ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-    ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-}
-
-
 ESX = nil
 local PlayerData = {}
 
@@ -97,7 +84,7 @@ end)
 
 RegisterNUICallback('enable-parking', function(data, cb)
     
-    local vehicles = ESX.Game.GetVehiclesInArea(GetEntityCoords(GetPlayerPed(-1)), 25.0)
+    local vehicles = ESX.Game.GetVehiclesInArea(GetEntityCoords(GetPlayerPed()), 25.0)
 
     for key, value in pairs(vehicles) do
         ESX.TriggerServerCallback('fd_impound:isOwned', function(owned)
@@ -120,7 +107,7 @@ RegisterNUICallback('park-out', function(data, cb)
         local x,y,z = table.unpack(garages[usedGarage][2])
         local props = json.decode(vehicle[1].vehicle)
 
-    local vehicles = ESX.Game.GetVehiclesInArea(GetEntityCoords(GetPlayerPed(-1)), 25.0)
+    local vehicles = ESX.Game.GetVehiclesInArea(GetEntityCoords(GetPlayerPed()), 25.0)
 
     for key, value in pairs(vehicles) do
         if GetVehicleNumberPlateText(value) == data.plate then
@@ -136,7 +123,7 @@ RegisterNUICallback('park-out', function(data, cb)
         }, garages[usedGarage][3], function(callback_vehicle)
             ESX.Game.SetVehicleProperties(callback_vehicle, props)
             SetVehRadioStation(callback_vehicle, "OFF")
-            TaskWarpPedIntoVehicle(GetPlayerPed(-1), callback_vehicle, -1)
+            TaskWarpPedIntoVehicle(GetPlayerPed(), callback_vehicle, -1)
         end)
 
     end, data.plate)
@@ -148,7 +135,7 @@ end)
 
 RegisterNUICallback('park-in', function(data, cb)
     
-    local vehicles = ESX.Game.GetVehiclesInArea(GetEntityCoords(GetPlayerPed(-1)), 25.0)
+    local vehicles = ESX.Game.GetVehiclesInArea(GetEntityCoords(GetPlayerPed()), 25.0)
 
     for key, value in pairs(vehicles) do
         if GetVehicleNumberPlateText(value) == data.plate then
@@ -166,7 +153,7 @@ Citizen.CreateThread(function()
         Wait(0)
 
         for key, value in pairs(garages) do
-            local dist = GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), value[1])
+            local dist = GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed()), value[1])
 
             if dist <= 2.0 then
                 ESX.ShowHelpNotification("Drücke ~INPUT_CONTEXT~ um auf dem Abschlepphof zuzugreifen")
